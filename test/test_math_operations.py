da264b4157e23207e09911876fdff6f55c85c59c
Law Bind Pandey <lawbindpandey01w@gmail.com>
2025-12-28 12:28:02 +0530

    fixed errors in math_operations.py

--- test/test_math_operations.py
+++ test/test_math_operations.py
@@ -1,7 +1,11 @@
 from src.math_operations import add, sub
 
-def test_add(a, b):
-    assert add(a, b) == a + b
+def test_add():
+    assert add(2, 3) == 5
 
-def test_sub(a, b):
-    assert sub(a, b) == a - b
+def test_sub():
+    assert sub(5,2) == 3
+    assert sub(5,3) == 2
+    assert sub(4,5) == -1
+    assert sub(3, 5) == -2
+    assert sub(5, 5) == 0
